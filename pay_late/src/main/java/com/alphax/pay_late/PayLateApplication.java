4ca3cc14da773ac1562b0a441d0b614e9bd9df64
Dhruv <dhruv.rathore@blackbuck.com>
2019-12-07 18:24:14 +0530

    initial commit

---
+++ pay_late/src/main/java/com/alphax/pay_late/PayLateApplication.java
@@ -0,0 +1,13 @@
+package com.alphax.pay_late;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayLateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayLateApplication.class, args);
+	}
+
+}
---
+++ pay_late/src/test/java/com/alphax/pay_late/PayLateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphax.pay_late;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayLateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
